7b135637f8ae2f9b8d0ec70e0267530615269bfc
Yuri Wingester <yuri2jwingester@gmail.com>
2016-10-27 14:13:46 +0200

    conflitos

---
+++ resources/views/evento/evento_create.blade.php
@@ -0,0 +1,55 @@
+@extends('layout.app')
+@section('content')
+<div class="container mukheroHack2">
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Criando novo evento</div>
+					@if (count($errors) > 0)
+						    <div class="alert alert-danger">
+						        <ul>
+						            @foreach ($errors->all() as $error)
+						                <li>{{ $error }}</li>
+						            @endforeach
+						        </ul>
+						    </div>
+						@endif
+                <div class="panel-body">
+                    {!!Form::open(array('url'=>'evento')) !!}
+	                    <div class="form-group">
+	                    	{{ Form::label('nome', 'Nome',  ['class' => 'label label-default','for' => 'nome_text' ])  }} 
+	                    	{{ Form::text('nome_text',null,['class' => 'form-control ', 'id' => 'nome_text']) }}
+	                    </div>
+	                    <div class="form-group">
+	                    	{{ Form::label('local', 'Local',  ['class' => 'label label-default','for' => 'local_text' ])  }} 
+	                    	{{ Form::text('local_text',null,['class' => 'form-control ', 'id' => 'local_text']) }}
+	                    </div>
+  						<div class="form-group">
+	                    	{{ Form::label('data_hora', 'Data e Hora',  ['class' => 'label label-default','for' => 'data_hora_text' ])  }} 
+	                    	{{ Form::text('data_hora_text',null,['class' => 'form-control ', 'id' => 'data_hora_text']) }}
+	                    </div>
+	                     <div class="form-group">
+	                    	{{ Form::label('descricao', 'Descricao',  ['class' => 'label label-default','for' => 'descricao_text' ])  }} 
+	                    	{{ Form::textarea('descricao_text',null,['class' => 'form-control ', 'id' => 'descricao_text']) }}
+	                    </div>
+	                    <div class="form-group">
+	                    	{{ Form::label('grupo_id', 'Grupo',  ['class' => 'label label-default','for' => 'grupo_dropdown' ])  }} 
+	                    	{!! Form::select('grupo_dropdown',$grupos, null, ['class' => 'form-control ', 'id' => 'grupo_dropdown']) !!}
+	                   </div>
+	                   <div>
+	                   		{!!Form::submit('Confirmar',['class' => 'btn btn-success pull-right']); !!}
+	                   </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<link href="{{ asset('css/jquery.datetimepicker.min.css') }}" rel="stylesheet">
+<script src="{{asset('js/jquery.datetimepicker.full.min.js')}}"></script>
+
+<script type="text/javascript">
+$('#data_hora_text').datetimepicker();
+</script>
+@endsection
---
+++ resources/views/evento/evento_show.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.app')
+@section('content')
+<div class="container mukheroHack2">
+    <div class="row">
+
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Visualizar Evento</div>
+                <div class="panel-body">
+                    {!!Form::open(array('url'=>'evento')) !!}
+	                    <div class="form-group">
+	                    	{{ Form::label('nome', 'Nome',  ['class' => 'label label-default','for' => 'nome_text' ])  }} 
+	                    	{{ Form::text('nome_text',$evento->nome,['class' => 'form-control ', 'id' => 'nome_text','readonly']) }}
+	                    </div>
+	                    <div class="form-group">
+	                    	{{ Form::label('local', 'Local',  ['class' => 'label label-default','for' => 'local_text' ])  }} 
+	                    	{{ Form::text('local_text',$evento->local,['class' => 'form-control ', 'id' => 'local_text','readonly']) }}
+	                    </div>
+  						<div class="form-group">
+	                    	{{ Form::label('data_hora', 'Data e Hora',  ['class' => 'label label-default','for' => 'data_hora_text' ])  }} 
+	                    	{{ Form::text('data_hora_text',$evento->data_hora,['class' => 'form-control ', 'id' => 'data_hora_text','readonly']) }}
+	                    </div>
+	                     <div class="form-group">
+	                    	{{ Form::label('descricao', 'Descricao',  ['class' => 'label label-default','for' => 'descricao_text' ])  }} 
+	                    	{{ Form::textarea('descricao_text',$evento->descricao,['class' => 'form-control ', 'id' => 'descricao_text','readonly']) }}
+	                    </div>
+	                    <div class="form-group">
+	                    	{{ Form::label('grupo_id', 'Grupo',  ['class' => 'label label-default','for' => 'grupo_dropdown' ])  }} 
+	                 		{{ Form::text('grupo_dropdown',$evento->grupo_id,['class' => 'form-control ', 'id' => 'grupo_dropdown','readonly']) }}
+	                   </div>
+	                   <div>
+	                   		{!!Form::submit('Editar',['class' => 'btn btn-success pull-right']); !!}
+	                   		{!!Form::submit('Ver Bilhetes',['class' => 'btn btn-success pull-right']); !!}
+	                   </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<link href="{{ asset('css/jquery.datetimepicker.min.css') }}" rel="stylesheet">
+<script src="{{asset('js/jquery.datetimepicker.full.min.js')}}"></script>
+
+<script type="text/javascript">
+$('#data_hora_text').datetimepicker();
+</script>
+@endsection
