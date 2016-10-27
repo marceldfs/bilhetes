58e5ac4534652569db546fc29563fabc4e542c44
Marcel Saraiva <mdfsaraiva@gmail.com>
2016-10-27 12:34:18 +0200

    Conclusao do crud de Grupo

--- resources/views/grupo/edit.blade.php
+++ resources/views/grupo/edit.blade.php
@@ -1,8 +1,30 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@extends('layout.app')
 
+@section('content')
+
+<div class="container mukheroHack1">
+<h1>Editar {{ $grupo->descricao }}</h1>
+
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+{{ Form::model($grupo, array('route' => array('grupo.update', $grupo->id), 'method' => 'PUT')) }}
+
+    <div class="form-group">
+        {{ Form::label('descricao', 'Descricao') }}
+        {{ Form::text('descricao', null, array('class' => 'form-control')) }}
+    </div>
+
+    {{ Form::submit('Editar grupo', array('class' => 'btn btn-primary')) }}
+
+{{ Form::close() }}
+
+</div>
--- resources/views/grupo/index.blade.php
+++ resources/views/grupo/index.blade.php
@@ -22,18 +22,16 @@
         <tr>
             <td>{{ $value->id }}</td>
             <td>{{ $value->descricao }}</td>
-
-            <!-- we will also add show, edit, and delete buttons -->
             <td>
 
-                <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
-                <!-- we will add this later since its a little more complicated than the other two buttons -->
+                {{ Form::open(array('url' => 'grupo/' . $value->id, 'class' => 'pull-right')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Apagar', array('class' => 'btn btn-warning')) }}
+                {{ Form::close() }}
+                
+                <a class="btn btn-small btn-success" href="{{ URL::to('grupo/' . $value->id) }}">Visualizar</a>
 
-                <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
-                <a class="btn btn-small btn-success" href="{{ URL::to('grupo/' . $value->id) }}">Visualizar grupo</a>
-
-                <!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->
-                <a class="btn btn-small btn-info" href="{{ URL::to('grupo/' . $value->id . '/edit') }}">Editar grupo</a>
+                <a class="btn btn-small btn-info" href="{{ URL::to('grupo/' . $value->id . '/edit') }}">Editar</a>
 
             </td>
         </tr>
