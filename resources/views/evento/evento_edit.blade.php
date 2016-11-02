10ca2956cbae7ff6fefefe6fe7c91b8cb1d48993
Yuri Wingester <yuri2jwingester@gmail.com>
2016-11-02 08:26:18 +0200

    editar e visualizar evento telas

--- resources/views/evento/evento_edit.blade.php
+++ resources/views/evento/evento_edit.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 <div class="container mukheroHack3">
     <div class="row">
-
-        <div class="col-md-8 col-md-offset-2 back-color-white">
+        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Editando o evento</div>
+            	 <div class="panel-heading panel-red-heading"><span class="text-white-color-legend">Editar Evento</span></div>
+            
 					@if (count($errors) > 0)
 						    <div class="alert alert-danger">
 						        <ul>
@@ -18,27 +18,29 @@
                 <div class="panel-body">
                     {!!Form::model($evento, array('route'=>array('evento.update', $evento->id), 'method'=>'PUT')) !!}
 	                    <div class="form-group">
-	                    	{{ Form::label('nome_label', 'Nome',  ['class' => 'label label-default','for' => 'nome' ])  }} 
+	                    	{{ Form::label('nome_label', 'Nome :',  ['class' => 'label-format pull-left','for' => 'nome' ])  }} 
 	                    	{{ Form::text('nome',null,['class' => 'form-control ', 'id' => 'nome']) }}
 	                    </div>
 	                    <div class="form-group">
-	                    	{{ Form::label('local_label', 'Local',  ['class' => 'label label-default','for' => 'local' ])  }} 
+	                    	{{ Form::label('local_label', 'Local :',  ['class' => 'label-format pull-left','for' => 'local' ])  }} 
 	                    	{{ Form::text('local',null,['class' => 'form-control ', 'id' => 'local']) }}
 	                    </div>
   						<div class="form-group">
-	                    	{{ Form::label('data_hora_label', 'Data e Hora',  ['class' => 'label label-default','for' => 'data_hora' ])  }} 
+	                    	{{ Form::label('data_hora_label', 'Data e Hora :',  ['class' => 'label-format pull-left','for' => 'data_hora' ])  }} 
 	                    	{{ Form::text('data_hora',null,['class' => 'form-control ', 'id' => 'data_hora']) }}
 	                    </div>
 	                     <div class="form-group">
-	                    	{{ Form::label('descricao_label', 'Descricao',  ['class' => 'label label-default','for' => 'descricao' ])  }} 
+	                    	{{ Form::label('descricao_label', 'Descrição "',  ['class' => 'label-format pull-left','for' => 'descricao' ])  }} 
 	                    	{{ Form::textarea('descricao',null,['class' => 'form-control ', 'id' => 'descricao']) }}
 	                    </div>
 	                    <div class="form-group">
-	                    	{{ Form::label('grupo_id_label', 'Grupo',  ['class' => 'label label-default','for' => 'grupo_dropdown' ])  }} 
+	                    	{{ Form::label('grupo_id_label', 'Grupo :',  ['class' => 'label-format pull-left','for' => 'grupo_dropdown' ])  }} 
 	                    	{!! Form::select('grupo_id',$grupos, null, ['class' => 'form-control ', 'id' => 'grupo_id']) !!}
 	                   </div>
 	                   <div>
-	                   		{!!Form::submit('Confirmar',['class' => 'btn btn-success pull-right']); !!}
+	                   		<button type="submit" class="btn btn-success btn-lg pull-right" aria-label="Left Align">
+  								<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>&nbsp;
+							</button>
 	                   </div>
                     {!! Form::close() !!}
                 </div>
