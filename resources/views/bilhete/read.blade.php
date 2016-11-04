b8a2f618c9df0f392e1706d28b283f0d1777b78d
Marcel Saraiva <mdfsaraiva@gmail.com>
2016-11-04 18:55:16 +0200

    Aumento da fonte da mensagem sobre estado do ticket para melhorar a visibilidade.

--- resources/views/bilhete/read.blade.php
+++ resources/views/bilhete/read.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.app')
 @section('content')
 <div class="container mukheroHack3">
-@if (Session::has('mensagem'))
-    <div class="alert alert-info">{{ Session::get('mensagem') }}</div>
-@endif
+    @if (Session::has('mensagem'))
+        <div class="alert alert-info"><h2>{{ Session::get('mensagem') }}</h2></div>
+    @endif
 </div>
 @endsection
 
