289e8b45ba0f3254b1e0400e4085d26128fdac98
Yuri Wingester <yuri2jwingester@gmail.com>
2016-11-12 14:18:08 +0200

    add do migrate para table contacto

---
+++ database/migrations/2016_11_12_121457_create_contacto_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacto', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('grupo_id')->unsigned();
+            $table->string('nome',20);
+            $table->string('numero',13);
+            $table->foreign('grupo_id')->references('id')->on('grupo');
+            $table->timestamps();
+        });
+ //
+   }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacto');
+    }
+}
